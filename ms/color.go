573a29f8985d072cde9c383d4522760111a447ba
k8 <make.me.loco@gmail.com>
2022-05-14 01:46:15 +0300

    edu: book - p.125.

--- ms/color.go
+++ ms/color.go
@@ -2,13 +2,18 @@ package main
 
 import "fmt"
 
-func paintNeeded(width float64, height float64) {
+var metersPerLiter float64
+
+func paintNeeded(width, height float64) float64 {
 	area := width * height
-	fmt.Printf("%.2f liters needed\n", area/10.0)
+	//fmt.Printf("%.2f liters needed\n", area/10.0)
+	return area / metersPerLiter
 }
 
 func main() {
-	paintNeeded(4.2, 3.0)
+	metersPerLiter = 10.0
+	fmt.Printf("%.2f\n", paintNeeded(4.2, 3.0))
+	/*paintNeeded(4.2, 3.0)
 	paintNeeded(5.2, 3.5)
-	paintNeeded(5.0, 3.3)
+	paintNeeded(5.0, 3.3)*/
 }
