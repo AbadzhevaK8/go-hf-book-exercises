7683310f5beedc81c4d3801567d22ce8aeea842e
k8 <make.me.loco@gmail.com>
2022-05-14 01:19:40 +0300

    edu: course - type Time, book - p.122.

--- ms/time.go
+++ ms/time.go
@@ -34,4 +34,28 @@ func main() {
 	fmt.Println(now.Format("02-01-2006 15:04:05"))         // 15-05-2020 09:58:16
 	fmt.Println(currentTime.Format("02-01-2006 15:04:05")) // 15-05-2020 10:13:12
 	fmt.Println(unixTime.Format("02-01-2006 15:04:05"))    // 02-01-1970 22:40:00
+	// func Parse(layout, value string) (Time, error)
+	// парсит дату и время в строковом представлении
+	firstTime, err := time.Parse("2006/01/02 15-04", "2020/05/15 17-45")
+	if err != nil {
+		panic(err)
+	}
+
+	// LoadLocation находит временную зону в справочнике IANA
+	// https://www.iana.org/time-zones
+	loc, err := time.LoadLocation("Asia/Yekaterinburg")
+	if err != nil {
+		panic(err)
+	}
+
+	// func ParseInLocation(layout, value string, loc *Location) (Time, error)
+	// парсит дату и время в строковом представлении с отдельным указанием временной зоны
+	secondTime, err := time.ParseInLocation("Jan 2 06 03:04:05pm", "May 15 20 05:45:10pm", loc)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(firstTime.Format("02-01-2006 15:04:05"))  // 15-05-2020 17:45:00
+	fmt.Println(secondTime.Format("02-01-2006 15:04:05")) // 15-05-2020 17:45:10
+
 }
