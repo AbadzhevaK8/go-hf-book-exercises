6e8bdeacbd7aeeb0655dff23b25998756bcdd233
k8 <make.me.loco@gmail.com>
2022-05-15 21:15:07 +0300

    edu: book - p.132

--- ms/color.go
+++ ms/color.go
@@ -2,18 +2,26 @@ package main
 
 import "fmt"
 
-var metersPerLiter float64
-
-func paintNeeded(width, height float64) float64 {
+func paintNeeded(width, height float64) (float64, error) {
+	if width < 0 {
+		return 0, fmt.Errorf("a width of %0.2f is invalid", width)
+	}
+	if height < 0 {
+		return 0, fmt.Errorf("a height of %0.2f is invalid", height)
+	}
 	area := width * height
-	//fmt.Printf("%.2f liters needed\n", area/10.0)
-	return area / metersPerLiter
+	return area / 10.0, nil
 }
 
 func main() {
-	metersPerLiter = 10.0
-	fmt.Printf("%.2f\n", paintNeeded(4.2, 3.0))
-	/*paintNeeded(4.2, 3.0)
-	paintNeeded(5.2, 3.5)
-	paintNeeded(5.0, 3.3)*/
+	var total float64
+	amount, err := paintNeeded(4.2, 3.0)
+	fmt.Println(err)
+	fmt.Printf("%.2f liters needed\n", amount)
+	total += amount
+	amount, err = paintNeeded(5.2, 3.5)
+	fmt.Println(err)
+	fmt.Printf("%.2f liters needed\n", amount)
+	total += amount
+	fmt.Printf("Total: %0.2f liters\n", total)
 }
