3474fc89bba68e77f8cf37ef88d6356eda597a49
k8 <make.me.loco@gmail.com>
2022-05-17 23:41:31 +0300

    edu: book - p.141

--- ms/page140.go
+++ ms/page140.go
@@ -2,10 +2,17 @@ package main
 
 import "fmt"
 
+func createPointer() *float64 {
+	var myFloat = 98.5
+	return &myFloat
+}
+
 func main() {
 	var myInt int
 	var myIntPointer *int
 	myInt = 42
 	myIntPointer = &myInt
 	fmt.Println(*myIntPointer)
+	var myFloatPointer *float64 = createPointer()
+	fmt.Println(*myFloatPointer)
 }
