c2c27bac36cb90cdf5af70bd1a596ff2860047ee
k8 <make.me.loco@gmail.com>
2022-05-20 22:23:01 +0300

    Test atom

---
+++ ms/src/main.go
@@ -0,0 +1,18 @@
+// tocelsius - converts temperature in degrees Fahrenheit to degrees Celsius.
+package main
+
+import (
+	"fmt"
+	"keyboard/keyboard"
+	"log"
+)
+
+func main() {
+	fmt.Print("Enter a temperature in Fahrenheit: ")
+	fahrenheit, err := keyboard.GetFloat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	celsius := (fahrenheit - 32) * 5 / 9
+	fmt.Printf("%0.2f degrees Celsius\n", celsius)
+}
